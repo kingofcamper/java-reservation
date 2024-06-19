14e7f9e70aadd402bcbe05e7cc26ca501f6944e0
kingofcamper <bill_bel@hotmail.com>
2024-06-19 09:23:12 +0100

    feat: Add Rest Api CRUD for user

--- src/main/java/com/tekup/travel_agency/controller/UserController.java
+++ src/main/java/com/tekup/travel_agency/controller/UserController.java
@@ -1,11 +1,12 @@
 package com.tekup.travel_agency.controller;
 
+import com.tekup.travel_agency.exception.ResourceNotFoundException;
 import com.tekup.travel_agency.model.User;
 import com.tekup.travel_agency.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,5 +20,36 @@ public class UserController {
     @GetMapping
     public List<User> getAllUsers() {
         return userRepository.findAll();
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<User> getUserById(@PathVariable long id) {
+        User user = userRepository.findById(id).orElseThrow(()-> new ResourceNotFoundException("User with id " + id + " not found"));
+        return ResponseEntity.ok(user);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<User> updateUser(@PathVariable long id, @RequestBody User userDetail) {
+        User updateUser = userRepository.findById(id).orElseThrow(()-> new ResourceNotFoundException("User with id " + id + " not found"));
+
+        updateUser.setFirstName(userDetail.getFirstName());
+        updateUser.setLastName(userDetail.getLastName());
+        updateUser.setEmail(userDetail.getEmail());
+
+        userRepository.save(updateUser);
+
+        return ResponseEntity.ok(updateUser);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<HttpStatus> deleteUser(@PathVariable long id) {
+        User user = userRepository.findById(id).orElseThrow(()-> new ResourceNotFoundException("User with id " + id + " not found"));
+        userRepository.delete(user);
+        return ResponseEntity.ok(HttpStatus.NO_CONTENT);
     }
 }
